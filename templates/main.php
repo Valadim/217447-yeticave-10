a562018e5e16291bb9ae2224ea8568b5737fcffc
Vadim <sakeyev@mail.ru>
2019-08-19 22:11:14 +0600

    Сделал вывод по шаблонам

---
+++ templates/main.php
@@ -0,0 +1,25 @@
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
+        горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <!--заполните этот список из массива категорий-->
+        <?php foreach ($categories as $key => $val): ?>
+            <li class="promo__item promo__item--<?= $key ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= $val ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <!--заполните этот список из массива с товарами-->
+        <?php foreach ($ads as $key => $val): ?>
+            <?= include_template('lot.php', ['val' => $val]); ?>
+        <?php endforeach; ?>
+    </ul>
+</section>
+
