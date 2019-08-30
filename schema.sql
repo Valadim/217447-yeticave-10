28f2e1ee774d20bcfded5c330ce2f4221b736d93
Vadim <sakeyev@mail.ru>
2019-08-30 21:37:02 +0600

    Поправил sql файл

--- schema.sql
+++ schema.sql
@@ -1,145 +1,43 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3307
--- Время создания: Авг 30 2019 г., 17:58
--- Версия сервера: 5.7.25
--- Версия PHP: 7.3.2
+CREATE DATABASE yeticave
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+USE yeticave;
 
+CREATE TABLE category (
+  cat_id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR NOT NULL,
+  class CHAR
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE lot (
+  lot_id INT AUTO_INCREMENT PRIMARY KEY,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  name  CHAR NOT NULL,
+  description TEXT,
+  img_path CHAR,
+  start_price INT NOT NULL,
+  finish_date DATETIME NOT NULL,
+  bid_step INT NOT NULL,
+  user_id INT,
+  cat_id INT
+);
 
---
--- База данных: `yeticave`
---
+CREATE TABLE bid (
+  bid_id INT AUTO_INCREMENT PRIMARY KEY,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  bid INT,
+  user_id INT,
+  lot_id INT
+);
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `bid`
---
-
-CREATE TABLE `bid` (
-  `bid_id` int(11) NOT NULL,
-  `bid_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_bid` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `lot_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `category`
---
-
-CREATE TABLE `category` (
-  `cat_id` int(11) NOT NULL,
-  `name` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `lot`
---
-
-CREATE TABLE `lot` (
-  `lot_id` int(11) NOT NULL,
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` char(1) NOT NULL,
-  `description` text,
-  `img_path` char(1) DEFAULT NULL,
-  `start_price` int(11) NOT NULL,
-  `finish_date` datetime NOT NULL,
-  `bid_step` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `cat_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `email` char(1) NOT NULL,
-  `username` char(1) NOT NULL,
-  `password` char(1) NOT NULL,
-  `contacts` text,
-  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `lot_id` int(11) DEFAULT NULL,
-  `bid_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `bid`
---
-ALTER TABLE `bid`
-  ADD PRIMARY KEY (`bid_id`);
-
---
--- Индексы таблицы `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`cat_id`);
-
---
--- Индексы таблицы `lot`
---
-ALTER TABLE `lot`
-  ADD PRIMARY KEY (`lot_id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `bid`
---
-ALTER TABLE `bid`
-  MODIFY `bid_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `category`
---
-ALTER TABLE `category`
-  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `lot`
---
-ALTER TABLE `lot`
-  MODIFY `lot_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE users (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  email CHAR NOT NULL,
+  username  CHAR NOT NULL,
+  password CHAR NOT NULL,
+  contacts TEXT,
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  lot_id INT,
+  bid_id INT
+);
