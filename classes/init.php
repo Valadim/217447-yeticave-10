204244c93e254bc3b309e0dbe9fa2ce4535faef8
Vadim <sakeyev@mail.ru>
2019-09-03 00:53:40 +0600

    Продолжил дз№5

--- classes/init.php
+++ classes/init.php
@@ -3,7 +3,7 @@ require_once 'functions.php';
 $db = require_once '../config/db.php';
 
 var_dump($db);
-$con = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+$con = mysqli_connect($db['host'], 'root', $db['password'], $db['database']);
 mysqli_set_charset($con, "utf8");
 
 if ($con == false) {
@@ -14,14 +14,3 @@ if ($con == false) {
 
 $categories = [];
 $content = '';
-
-
-//$con = mysqli_connect("localhost", "root", "", "yeticave");
-//mysqli_set_charset($con, "utf8");
-//
-//if ($con == false) {
-//    print(" Ошибка подключения: " . mysqli_connect_error());
-//} else {
-//    print(" Соединение установлено");
-//    // выполнение запросов
-//}
