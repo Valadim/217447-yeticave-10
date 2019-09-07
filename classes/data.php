cc8a34f534dea70023b8d5ee1428bc20fee2638b
Vadim <sakeyev@mail.ru>
2019-09-07 15:28:23 +0600

    Я уже не понимаю что творю, но необходимо продолжать

--- classes/data.php
+++ classes/data.php
@@ -10,7 +10,10 @@ if (!$con) {
     $sql = 'SELECT `class`, `name` FROM category';
     $result = mysqli_query($con, $sql);
 
-    $sql_lot = 'SELECT `id`, `date`, `name`, `description`, `img_path`, `start_price`, `finish_date`, `bid_step`, `user_id`, `category_id`, `is_active` FROM lot';
+    $sql_lot_join = 'SELECT c.name FROM category c JOIN lot l ON c.name = l.category_id';
+    $result_lot_join = mysqli_query($con, $sql_lot_join);
+
+    $sql_lot = 'SELECT `id`, `date`, `name`, `description`, `img_path`, `start_price`, `finish_date`, `bid_step`, `user_id`, `category_id`, `is_active` FROM lot'  ;
     $result_lot = mysqli_query($con, $sql_lot);
 
     if ($result && $result_lot) {
@@ -22,3 +25,5 @@ if (!$con) {
         $content = include_template('error.php', ['error' => $error]);
     }
 }
+
+//  SELECT c.name FROM category c JOIN lot l ON c.name = l.category_id;
